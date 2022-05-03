d50b81060b93098ded1bb131fc4b7b045c4c780b
kenzy yacini <91525930+Eberlouedd@users.noreply.github.com>
2022-05-03 22:20:35 +0200

    Add files via upload

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: kyacini <kyacini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:11:54 by kyacini           #+#    #+#             */
-/*   Updated: 2022/05/03 14:26:54 by kyacini          ###   ########.fr       */
+/*   Updated: 2022/05/03 21:44:53 by kyacini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ void	*ft_calloc(size_t elementCount, size_t elementSize)
 {
 	void			*retur;
 	unsigned char	*new;
-	int				i;
+	size_t			i;
 
+	retur = NULL;
 	i = 0;
 	new = retur;
 	new = malloc(elementCount * elementSize);
